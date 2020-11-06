994e64c34840ea4a425039dfd4c5e267639d01f0
lamedoo <kordzaialuka@gmail.com>
2020-11-06 22:26:24 +0400

    HelpMeApp-5 Test Issue

--- app/src/main/java/com/lukakordzaia/helpmeapp/ui/userprofile/useraddresses/UserAddressesFragment.kt
+++ app/src/main/java/com/lukakordzaia/helpmeapp/ui/userprofile/useraddresses/UserAddressesFragment.kt
@@ -2,11 +2,8 @@ package com.lukakordzaia.helpmeapp.ui.userprofile.useraddresses
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.lukakordzaia.helpmeapp.R
