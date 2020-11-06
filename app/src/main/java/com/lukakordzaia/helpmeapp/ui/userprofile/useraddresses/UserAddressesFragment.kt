51cd90b20c2b77ff73bbe2067f48502c1f0d0f19
lamedoo <kordzaialuka@gmail.com>
2020-11-06 22:16:08 +0400

    TestIssue resolved

--- app/src/main/java/com/lukakordzaia/helpmeapp/ui/userprofile/useraddresses/UserAddressesFragment.kt
+++ app/src/main/java/com/lukakordzaia/helpmeapp/ui/userprofile/useraddresses/UserAddressesFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.lukakordzaia.helpmeapp.R
@@ -22,6 +23,10 @@ class UserAddressesFragment : Fragment(R.layout.fragment_user_addresses) {
 
         viewModel.addressExists.observe(viewLifecycleOwner, Observer {
             user_address_container.setVisibleOrGone(it)
+            if (it) {
+                Toast.makeText(context, "this is a toast", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "this is a second toast", Toast.LENGTH_SHORT).show()
+            }
         })
 
     }
